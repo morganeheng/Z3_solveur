e86e2ecb22a4c5b84fde47c999a912a1201bfe65
Morgane Heng <heng.morgane@hotmail.fr>
2019-07-23 11:59:28 +0200

    init repo

---
+++ TestZ3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Z3_solveur;
+using SudokuLib;
+using System.Collections.Generic;
+
+namespace TestZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Z3 solveur = new Z3();
+            Sudoku s = new Sudoku();
+            List<Sudoku> sudokus = new List<Sudoku>();
+            sudokus = s.ParseFile("C:/Users/hengm/Downloads/Easy.txt");
+            for (int i = 0; i < sudokus.Count; i++)
+            {
+                s = solveur.Solve(sudokus[i]);
+                Console.WriteLine("Solution : ---------------\n");
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Z3_solveur/Z3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SudokuLib;
+
+namespace Z3_solveur
+{
+    public class Z3 : ISudokuSolver
+    {
+        public Sudoku Solve(Sudoku sudoku)
+        {
+            Console.WriteLine(sudoku);
+
+            //Ajouter le code de la resolution du sudoku
+
+            return sudoku;
+        }
+    }
+}
